69fbeb799d85d0994c71f9719cc54b9298b6651c
11VladC11 <vladw239@gmail.com>
2022-07-05 16:18:41 +0300

    full redux migration from context

--- src/index.js
+++ src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import App from './App';
-import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
-
 import './index.scss';
-
 const rootElement = document.getElementById('root');
-
 render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,12 +1,18 @@
-export const selectCategoriesMap = (state) => {
-	console.log('first', state.categories.categories)
+import { createSelector } from 'reselect';
 
-  const categoriesMap = state.categories.categories.reduce(
-    (acc, { title, items }) => {
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    },
-    {}
-  );
-  return categoriesMap;
-};
+    }, {})
+);
